8f6ac3339d6e10eba976dcc7fa48b3c7dfd8b2d1
cynos07 <cynos07@naver.com>
2017-04-12 22:57:59 +0900

    Project Initialized

---
+++ src/main/groovy/kr/studygram/androidPresentation/groovyTest.java
@@ -0,0 +1,11 @@
+package kr.studygram.androidPresentation;
+
+/**
+ * Created by cynos07 on 2017-04-12.
+ */
+public class groovyTest {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
---
+++ src/main/groovy/kr/studygram/androidPresentation/groovyTest2.groovy
@@ -0,0 +1,10 @@
+package kr.studygram.androidPresentation
+
+/**  * Created by cynos07 on 2017-04-12.
+ */
+class groovyTest2 {
+    static void main(args)
+    {
+        println 'Hello World';
+    }
+}
